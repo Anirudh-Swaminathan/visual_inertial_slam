e80b4460e18975bc3c11c1a9f1e195a68a43cb0c
Anirudh Swaminathan <aniswami97@gmail.com>
2020-03-07 01:40:47 -0800

    Run dead reckoning code on all datasets
    Modify code accordingly to accommodate pictures with and without the
    indicating blue arrows

--- src/hw3_main.py
+++ src/hw3_main.py
@@ -3,14 +3,14 @@ from utils import *
 from imu import IMU
 
 if __name__ == '__main__':
-    filename = "./data/0027.npz"
+    filename = "./data/0034.npz"
     t, features, linear_velocity, rotational_velocity, K, b, cam_T_imu = load_data(filename)
     # print(linear_velocity.shape)
     # print(rotational_velocity.shape)
 
     # instantiate my IMU class
     ani_imu = IMU()
-    save_base = "./outputs/dead_reckoning/d0027/try_002/"
+    save_base = "./outputs/dead_reckoning/d0034/try_002/"
 
     # loop through all time steps
     for ti in range(1, t.shape[1]):
@@ -47,7 +47,7 @@ if __name__ == '__main__':
             assert (world_T_imu.shape[1] == 4)
             assert(np.sum(np.abs(old_start - new_start)) <= 1e-6)
             # assert (world_T_imu.shape[2] == t.shape[1])
-            visualize_trajectory_2d(world_T_imu, path_name="dr_0027", show_ori=True,
+            visualize_trajectory_2d(world_T_imu, path_name="dr_0034", show_ori=True,
                                     save_pth=save_base + "path_img_" + str(ti) + ".png")
 
     # final save
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     assert (world_T_imu.shape[1] == 4)
     assert (np.sum(np.abs(old_start - new_start)) <= 1e-6)
     # assert (world_T_imu.shape[2] == t.shape[1])
-    visualize_trajectory_2d(world_T_imu, path_name="dr_0027", show_ori=True,
+    visualize_trajectory_2d(world_T_imu, path_name="dr_0034", show_ori=True,
                             save_pth=save_base + "path_img_" + str(t.shape[1]) + ".png")
 
     # (a) IMU Localization via EKF Prediction
