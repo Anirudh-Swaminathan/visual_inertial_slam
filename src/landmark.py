7dcb2fcae6e185c2358b8316156dec55bb7c912f
Anirudh Swaminathan <aniswami97@gmail.com>
2020-03-10 19:59:23 -0700

    Visual Mapping makes sense
    Need to overlay with dead reckoning to compare differences.

--- src/landmark.py
+++ src/landmark.py
@@ -17,6 +17,7 @@ class LandMarks(object):
         self.M = m
         self.means = np.zeros((3 * self.M, 1))
         self.covs = np.zeros((3 * self.M, 3 * self.M))
+        # self.covs = np.identity(3*self.M)
 
         # boolean array to keep track of all the observed landmarks till time t
         self.observed_inds = np.array([False for _ in range(self.M)])
@@ -29,6 +30,9 @@ class LandMarks(object):
 
         # boolean array of indices to initialize mean for at time t
         self.cur_init = np.array([False for _ in range(self.M)])
+
+        # the observation model noise
+        self.V = np.identity(4)
 
     def _update_inds(self, z_t):
         """
@@ -47,11 +51,11 @@ class LandMarks(object):
         self.observed_inds = np.logical_or(self.current_inds, self.observed_inds)
 
         # DEBUG
-        print(self.current_inds)
-        print(self.cur_init)
-        print(self.observed_inds)
+        # print(self.current_inds)
+        # print(self.cur_init)
+        # print(self.observed_inds)
 
-    def _init_means(self, z_t, c_mat, cam_T_imu, imu_pose):
+    def _init_landmarks(self, z_t, c_mat, cam_T_imu, imu_pose):
         """
         Initialize the means for the newly observed landmarks using the inverse of Stereo Camera Observation model
         :param z_t: left and right camera pixel coordinates
@@ -59,20 +63,21 @@ class LandMarks(object):
         :param cam_T_imu: pose of IMU in Camera frame
         :return:
         """
-        print("In in _init_means")
-        print(np.sum(self.cur_init), np.sum(self.observed_inds))
+        print("In _init_means")
+        num_new = np.sum(self.cur_init)
+        print(num_new, np.sum(self.observed_inds), np.sum(self.current_inds))
 
         # return if no initializations are required
-        if np.sum(self.cur_init) == 0:
+        if num_new == 0:
             return
         print(z_t.shape)
         uls = z_t[0, self.cur_init]
         vls = z_t[1, self.cur_init]
         urs = z_t[2, self.cur_init]
         ds = uls - urs
-        assert(uls.size == np.sum(self.cur_init))
-        assert(vls.size == np.sum(self.cur_init))
-        assert(ds.size == np.sum(self.cur_init))
+        assert (uls.size == num_new)
+        assert (vls.size == num_new)
+        assert (ds.size == num_new)
 
         # To avoid division by 0 errors
         # ins = np.where(abs(ds) < 1e-6)
@@ -90,20 +95,140 @@ class LandMarks(object):
         opt_homo = np.vstack((opt_homo, np.ones((1, z.size))))
         # print(opt_homo.shape)
         assert (opt_homo.shape[0] == 4)
-        assert (opt_homo.shape[1] == np.sum(self.cur_init))
+        assert (opt_homo.shape[1] == num_new)
         assert (cam_T_imu.size == 16)
         imu_frame = np.matmul(np.linalg.inv(cam_T_imu), opt_homo)
         world_frame = np.matmul(imu_pose, imu_frame)
-        assert (world_frame.shape[1] == np.sum(self.cur_init))
+        assert (world_frame.shape[1] == num_new)
         mean_2d = np.copy(self.means.reshape((3, self.M), order='F'))
         assert (mean_2d.shape[0] == 3)
         assert (mean_2d.shape[1] == self.M)
         mean_2d[:, self.cur_init] = np.copy(world_frame[:3, :])
         self.means = np.copy(mean_2d.flatten('F').reshape(self.means.shape))
 
+        # Initialize the covariances of these landmarks
+        cov_init = np.identity(3 * num_new) * 1e-2
+        intinds = np.where(self.cur_init == True)[0]
+        # print(intinds)
+        for i in range(num_new):
+            self.covs[intinds[i] * 3: (intinds[i] + 1) * 3, intinds[i] * 3: (intinds[i] + 1) * 3] \
+                = np.copy(cov_init[i * 3: (i + 1) * 3, i * 3:(i + 1) * 3])
+
+    def _projective_derivative(self, q):
+        """
+        Returns the matrix representing the differentiation of the projection function
+        :param q: the homogeneous coordinate to take the derivative for
+        :return:
+        """
+        ret = np.identity(4)
+        ret[:, 2] -= 1.0 * q / q[2]
+        return 1.0/q[2] * ret
+
+    def _update_landmarks(self, z_t, c_mat, cam_T_imu, imu_pose):
+        """
+        Function to update the already observed landmarks in this time step
+        :param z_t: Current time observations
+        :param c_mat: Stereo Camera disparity matrix
+        :param cam_T_imu: Pose of IMU in camera frame
+        :param imu_pose: Pose of IMU in world frame
+        :return:
+        """
+        print("In Update landmarks!")
+        num_upds = np.sum(self.cur_upds)
+        print(num_upds, np.sum(self.observed_inds), np.sum(self.current_inds))
+        if num_upds == 0:
+            return
+        # Construct the required M - 4*4
+        M = np.zeros((4, 4))
+        M[:2, :] = c_mat[:2, :]
+        M[2:, :] = c_mat[:2, :]
+        M[2, 3] = -1.0 * c_mat[2, 3]
+
+        # compute Ut - 4*4
+        Ut = np.linalg.inv(imu_pose)
+        assert(M.shape == Ut.shape)
+
+        # Projection - 3*4
+        P = np.zeros((3, 4))
+        P[:, :3] = np.identity(3)
+
+        # TUt - 4*4
+        tut = np.matmul(cam_T_imu, Ut)
+        assert(tut.shape == M.shape)
+
+        # reshape the means
+        # mut - 3*M
+        mut = self.means.reshape((3, self.M), order='F')
+        ones = np.ones((1, self.M))
+        # mubt - 4*M
+        mubt = np.vstack((mut, ones))
+        assert(mubt.shape[0] == 4)
+        assert(mubt.shape[1] == self.M)
+
+        # compute the q inside the Jacobian
+        # q - 4*M
+        q = np.matmul(tut, mubt)
+        assert(q.shape == (4, self.M))
+        pq = q / q[2, :]
+        assert(pq.shape == (4, self.M))
+        z_tbar = np.matmul(M, pq)
+        assert(z_tbar.shape == (4, self.M))
+        assert(np.sum(np.abs(z_tbar[1, :] - z_tbar[3, :])) < 1e-6)
+
+        # set up Jacobian - 4Nt * 3M
+        Nt = np.sum(self.current_inds)
+        Ht = np.zeros((4*Nt, 3*self.M))
+        inds = np.where(self.cur_upds == True)[0]
+        print(inds)
+        for i in range(len(inds)):
+            dpq = self._projective_derivative(q[:, inds[i]])
+            assert(dpq.shape == M.shape)
+            r = np.matmul(tut, np.transpose(P))
+            mid = np.matmul(dpq, r)
+            htii = np.matmul(M, mid)
+            Ht[i*4:(i+1)*4, inds[i]*3:(inds[i]+1)*3] = np.copy(htii)
+
+        # Set up the covariance as a block diagonal
+        IV = np.identity(4*Nt)
+        sigmaH = np.matmul(self.covs, np.transpose(Ht))
+        assert(sigmaH.shape[0] == 3*self.M)
+        assert(sigmaH.shape[1] == 4*Nt)
+        hsh = np.matmul(Ht, sigmaH)
+        assert(hsh.shape == (4*Nt, 4*Nt))
+        inside_inv = hsh + IV
+        assert(IV.shape == (4*Nt, 4*Nt))
+        assert(inside_inv.shape == (4*Nt, 4*Nt))
+        ro = np.linalg.inv(inside_inv)
+        mido = np.matmul(np.transpose(Ht), ro)
+        assert(mido.shape == (3*self.M, 4*Nt))
+        Kt = np.matmul(self.covs, mido)
+        assert(Kt.shape == (3*self.M, 4*Nt))
+
+        # mean update
+        flat_zt = z_t[:, self.current_inds].flatten('F')
+        flat_zt_bar = z_tbar[:, self.current_inds].flatten('F')
+        # print(z_t.shape)
+        # print(z_tbar.shape)
+        # print(flat_zt.shape)
+        # print(flat_zt_bar.shape)
+        fzt = flat_zt.reshape(flat_zt.size, 1)
+        fztb = flat_zt_bar.reshape(flat_zt_bar.size, 1)
+        assert(fzt.shape == (4*Nt, 1))
+        assert(fztb.shape == (4*Nt, 1))
+        adt = np.matmul(Kt, (fzt - fztb))
+        assert(adt.shape == (3*self.M, 1))
+        self.means += np.copy(adt)
+
+        # covariance update
+        ktht = np.matmul(Kt, Ht)
+        assert(ktht.shape == (3*self.M, 3*self.M))
+        I = np.identity(3*self.M)
+        self.covs = np.matmul((I - ktht), self.covs)
+
     def update(self, z_t, c_mat, cam_T_imu, imu_pose):
         self._update_inds(z_t)
-        self._init_means(z_t, c_mat, cam_T_imu, imu_pose)
+        self._init_landmarks(z_t, c_mat, cam_T_imu, imu_pose)
+        self._update_landmarks(z_t, c_mat, cam_T_imu, imu_pose)
 
     def get_obs_means(self):
         """
@@ -115,12 +240,34 @@ class LandMarks(object):
         act_means = mean_2d[:, self.observed_inds]
         return act_means
 
+    def get_obs_covs(self):
+        """
+        Returns covariances of all observed landmarks so far
+        :return:
+        """
+        cov = np.copy(self.covs)
+        ret_inds = np.where(self.observed_inds == True)[0]
+        K = np.sum(self.observed_inds)
+        ret_mat = np.zeros((K * 3, K * 3))
+        for i in range(K):
+            ret_mat[i * 3:(i + 1) * 3, i * 3:(i + 1) * 3] = cov[ret_inds[i] * 3:(ret_inds[i] + 1) * 3,
+                                                            ret_inds[i] * 3:(ret_inds[i] + 1) * 3]
+        return ret_mat
+
     def get_means(self):
         """
         Returns means for all landmarks for saving
         :return:
         """
         ret = np.copy(self.means.reshape((self.M, 3)).T)
+        return ret
+
+    def get_covs(self):
+        """
+        Returns the covariances of all landmarks for saving
+        :return:
+        """
+        ret = np.copy(self.covs)
         return ret
 
     def _save_means(self, pth):
@@ -132,6 +279,15 @@ class LandMarks(object):
         sv = self.get_means()
         np.save(pth + "means.npy", sv)
 
+    def _save_covs(self, pth):
+        """
+        save the covariances of all landmarks to specified path
+        :param pth: path to save the covariances to
+        :return:
+        """
+        sv = self.get_covs()
+        np.save(pth + "covs.npy", sv)
+
     def save_landmarks(self, pth):
         """
         Save the means and the covariances to file
@@ -139,3 +295,4 @@ class LandMarks(object):
         :return:
         """
         self._save_means(pth)
+        self._save_covs(pth)
---
+++ src/visual_mapping.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+
+# Created by anicodebreaker on March 10, 2020
+
+import numpy as np
+from utils import *
+from imu import IMU
+from landmark import LandMarks as LMS
+
+if __name__ == '__main__':
+    filename = "./data/0027.npz"
+    t, features, linear_velocity, rotational_velocity, K, b, cam_T_imu = load_data(filename)
+    print(features.shape)
+    print(K.shape)
+    print(b)
+    print(cam_T_imu.shape)
+
+    # Extract every 10th feature for faster computation
+    ori_num_feats = features.shape[1]
+    new_inds = np.arange(0, ori_num_feats, 10)
+    new_feats = features[:, new_inds, :]
+    features = np.copy(new_feats)
+
+    # COMPUTE the Stereo Camera Calibration matrix
+    c_mat = np.zeros((3, 4))
+    c_mat[:, :3] = np.copy(K)
+    c_mat[2][2] = 0
+    c_mat[2][3] = K[0][0] * b
+    print(K)
+    print(c_mat)
+
+    save_base = "./outputs/visual_mapping/d0027/try_004/"
+
+    imu_base = "outputs/dead_reckoning/d0027/try_002/"
+    imu_poses = np.load(imu_base + "path_" + str(t.shape[1]) + ".npy")
+    imu_inv_poses = np.load(imu_base + "hist_" + str(t.shape[1]) + ".npy")
+    print(imu_poses.shape)
+
+    # recover the dead reckoned landmark points
+    dead_map_base = "outputs/visual_mapping_dr/d0027/try_001/"
+
+    # instantiate landmarks
+    # M - Number of landmarks
+    M = features.shape[1]
+    lms = LMS(M)
+
+    # check if the only negative value is -1
+    assert (len(np.unique(features[features < 0])) == 1)
+
+    # loop through all time steps
+    for ti in range(1, t.shape[1]):
+        # time discretization
+        tau = t[0][ti] - t[0][ti - 1]
+
+        # current IMU inverse pose and pose
+        cur_inv_pose = imu_inv_poses[ti, :, :]
+        cur_pose = imu_poses[ti, :, :]
+
+        # current observations
+        z_t = features[:, :, ti]
+        lms.update(z_t, c_mat, cam_T_imu, cur_pose)
+
+        if ti % 500 == 0 or ti == 1:
+            # save landmark means and covariances
+            lms.save_landmarks(save_base + "landmarks_" + str(ti) + "_")
+
+        if ti % 100 == 0 or ti == 1:
+            # You can use the function below to visualize the robot pose over time
+            world_T_imu = imu_poses[:ti, :, :]
+            old_start = world_T_imu[0, :, :]
+            # world_T_imu = world_T_imu.reshape((4, 4, ti + 1))
+            world_T_imu = np.moveaxis(world_T_imu, [0, 1, 2], [2, 0, 1])
+            new_start = world_T_imu[:, :, 0]
+            # print(world_T_imu.shape)
+            # print(old_start)
+            # print(new_start)
+            assert (world_T_imu.shape[0] == 4)
+            assert (world_T_imu.shape[1] == 4)
+            assert (np.sum(np.abs(old_start - new_start)) <= 1e-6)
+            # assert (world_T_imu.shape[2] == t.shape[1])
+            land_means = lms.get_obs_means()
+            land_covs = lms.get_obs_covs()
+            print("So far observed landmarks!!")
+            print("Covariances are shaped as ", land_covs.shape)
+            # print(land_covs)
+            visualize_trajectory_2d(world_T_imu, landmarks=land_means, path_name="mp_0027", show_ori=True,
+                                    save_pth=save_base + "map_img_" + str(ti) + ".png")
+
+    # final save
+    # save IMU inverse poses and history over all time steps
+    lms.save_landmarks(save_base + "landmarks_" + str(t.shape[1]) + "_")
+
+    # Final visualization
+    # You can use the function below to visualize the robot pose over time
+    world_T_imu = imu_poses[:, :, :]
+    old_start = world_T_imu[0, :, :]
+    # world_T_imu = world_T_imu.reshape((4, 4, ti + 1))
+    world_T_imu = np.moveaxis(world_T_imu, [0, 1, 2], [2, 0, 1])
+    new_start = world_T_imu[:, :, 0]
+    assert (world_T_imu.shape[0] == 4)
+    assert (world_T_imu.shape[1] == 4)
+    assert (np.sum(np.abs(old_start - new_start)) <= 1e-6)
+    # assert (world_T_imu.shape[2] == t.shape[1])
+    land_means = lms.get_obs_means()
+    visualize_trajectory_2d(world_T_imu, landmarks=land_means, path_name="mp_0027", show_ori=True,
+                            save_pth=save_base + "map_img_" + str(t.shape[1]) + ".png")
+
+    # (a) IMU Localization via EKF Prediction
+
+    # (b) Landmark Mapping via EKF Update
+
+    # (c) Visual-Inertial SLAM
