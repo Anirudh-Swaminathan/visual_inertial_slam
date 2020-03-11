f8e5df6fac12dba7cbb8a399cf95a46e70d0f0a6
Anirudh Swaminathan <aniswami97@gmail.com>
2020-03-11 13:26:39 -0700

    Visual Mapping for other datasets done
    Also started with predict mapping for IMU

---
+++ src/predict_mapping.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+# Created by anicodebreaker on March 11, 2020
--- src/visual_mapping.py
+++ src/visual_mapping.py
@@ -8,7 +8,7 @@ from imu import IMU
 from landmark import LandMarks as LMS
 
 if __name__ == '__main__':
-    filename = "./data/0027.npz"
+    filename = "./data/0034.npz"
     t, features, linear_velocity, rotational_velocity, K, b, cam_T_imu = load_data(filename)
     print(features.shape)
     print(K.shape)
@@ -29,15 +29,15 @@ if __name__ == '__main__':
     print(K)
     print(c_mat)
 
-    save_base = "./outputs/visual_mapping/d0027/try_004/"
+    save_base = "./outputs/visual_mapping/d0034/try_001/"
 
-    imu_base = "outputs/dead_reckoning/d0027/try_002/"
+    imu_base = "outputs/dead_reckoning/d0034/try_001/"
     imu_poses = np.load(imu_base + "path_" + str(t.shape[1]) + ".npy")
     imu_inv_poses = np.load(imu_base + "hist_" + str(t.shape[1]) + ".npy")
     print(imu_poses.shape)
 
     # recover the dead reckoned landmark points
-    dead_map_base = "outputs/visual_mapping_dr/d0027/try_001/landmarks_"
+    dead_map_base = "outputs/visual_mapping_dr/d0034/try_001/landmarks_"
     dead_map = np.load(dead_map_base + str(t.shape[1]) + "_means.npy")
     dead_map = dead_map[:, new_inds]
     print(dead_map.shape)
@@ -87,8 +87,8 @@ if __name__ == '__main__':
             print("So far observed landmarks!!")
             print("Covariances are shaped as ", land_covs.shape)
             # print(land_covs)
-            visualize_trajectory_2d(world_T_imu, dead_means=dead_map[:, :land_means.shape[1]], landmarks=land_means,
-                                    path_name="mp_0027", show_ori=True,
+            visualize_trajectory_2d(world_T_imu, landmarks=land_means,
+                                    path_name="mp_0034", show_ori=True,
                                     save_pth=save_base + "map_img_" + str(ti) + ".png")
 
     # final save
@@ -107,7 +107,7 @@ if __name__ == '__main__':
     assert (np.sum(np.abs(old_start - new_start)) <= 1e-6)
     # assert (world_T_imu.shape[2] == t.shape[1])
     land_means = lms.get_obs_means()
-    visualize_trajectory_2d(world_T_imu, dead_means=dead_map, landmarks=land_means, path_name="mp_0027", show_ori=True,
+    visualize_trajectory_2d(world_T_imu, landmarks=land_means, path_name="mp_0034", show_ori=True,
                             save_pth=save_base + "map_img_" + str(t.shape[1]) + ".png")
 
     # (a) IMU Localization via EKF Prediction
